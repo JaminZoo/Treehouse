9344bd32745ffe2629dd1a64ca1c84ad83e2cabc
JaminZoo <b.zhu86@gmail.com>
2015-08-14 14:44:13 +1000

    Other files for game

---
+++ character.py
@@ -0,0 +1,58 @@
+import random
+
+from combat import Combat
+
+
+class Character(Combat):
+
+# standard attributes that a character starts off with each time the game is run
+
+	max_attack = 10
+	experience = 0
+	base_hit_points = 10
+	
+# define attack method
+
+	def attack(self):
+		roll = random.randint(1, self.max_attack)
+		# if user selects sword for weapon, add one to max attack value return by roll
+		if self.weapon['type'] == 'sword':
+			roll += 2
+		if self.weapon['type'] == 'axe':
+			roll += 1
+		return roll > 4
+	
+# define select weapon method
+
+	def getWeapon(self):
+		weapon_choice = input('Choose your weapon: [S]word, [A]xe or [C]lub').lower()
+		
+		if weapon_choice in 'sac':
+			if weapon_choice == 's':
+				return {'type':'sword', 'damage':3}
+			elif weapon_choice == 'a':
+				return {'type':'axe', 'damage':2}
+			else:
+				return {'type':'club', 'damage':1}
+		else:
+			return self.getWeapon() # call getWeapon method if user inputs nothing or different selection
+	
+	
+	def __init__(self, **kwargs):
+		# user sees this every time the class is initialised i.e. at the start of the 'game'
+		self.name = input('What is your name oh brave soul? ')
+		self.weapon = self.getWeapon()
+		self.hit_points = self.base_hit_points
+		
+		for key, value in kwargs.items():
+			setattr(self, key, value)
+	
+	def __str__(self):
+		return '{}, HP: {}, EXP: {}'.format(self.name, self.hit_points, self.experience)
+	
+	def rest(self):
+		if self.hit_points < self.base_hit_points
+			self.hit_points += 1 # if player decides to rest for their turn, add one additional hit point to base score
+			
+	def levelUp(self):
+		return self.expeience >= 5 # when character reaches exp of 5 or more
---
+++ combat.py
@@ -0,0 +1,22 @@
+import random
+
+
+class Combat(self):
+# define attributes common for attack and dodge
+	max_attack = 6
+	max_dodge = 6
+
+
+# define attack behaviour
+
+	def attack(self):
+		roll = random.randint(1, self.max_attack)
+		print('Attack roll: {}'.format(roll))
+		return roll > 4 # only return output if value of roll is greater than 4
+	
+
+# define dodge behaviour
+	def dodge(self):
+		roll = random.randint(1, self.max_dodge)
+		print('Dodge roll: {}'.format(roll))
+		return roll > 4
